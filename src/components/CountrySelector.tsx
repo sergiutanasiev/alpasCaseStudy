c01f06dfd1bcc6bf711101ff7e143692c6cbbf72
Tanasiev Sergiu <tanasiev.sergiu@gmail.com>
2024-11-18 16:55:04 +0100

    selector component add input and state

--- src/components/CountrySelector.tsx
+++ src/components/CountrySelector.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useQuery } from '@tanstack/react-query';
 
 
 interface Country {
-    name: {};
+    name: {
+        common: string
+    };
     cca2: string;
     cca3: string;
 }
@@ -12,7 +14,6 @@ async function fetchCountries () {
     try {
         const response = await fetch('https://restcountries.com/v3.1/all');
         const data = await response.json();
-        console.log(data);
         return data;
     } catch(error) {
         console.log(error);
@@ -20,19 +21,40 @@ async function fetchCountries () {
 }
 
 function CountrySelector () {
+    const [inputQuery, setInputQuery] = useState<string>('');
+    const [allCountries, setAllCountries] = useState<Country[]>([]);
+    const [filteredCountries, setFilteredCountries] = useState<Country[]>([]);
+    const [highlightedIndex, setHighlitedIndex] = useState<number>(-1);
+    const [isVisible, setIsVisible] = useState<boolean>(false);
+    const inputRef = useRef<HTMLInputElement>(null);
 
-    const { data, isLoading } = useQuery({
+    const { data: countries, isLoading } = useQuery({
         queryKey: ['countries'],
         queryFn: () => fetchCountries()
     });
 
-    React.useEffect(() => {
+    useEffect(() => {
+        setAllCountries(countries || []);
+    })
 
-    }, [])
-
+    function handleInputChange(e: React.ChangeEvent<HTMLInputElement>) {
+        setInputQuery(e.target.value);
+    }
 
     return (
-        <div></div>
+        <div>
+            <input
+                value={inputQuery}
+                ref={inputRef} 
+                onChange={handleInputChange}
+                className={'country-input'}
+                type="text" />
+            <div>
+                {allCountries.map((el, index) => (
+                    <div>{el.name.common}</div>
+                ))}    
+            </div>    
+        </div>
     )
 }
 
